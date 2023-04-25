87c99b9b8102cbd22042da7bec4c46f2c68733a7
1King-coder <progvitor@gmail.com>
2023-04-25 09:10:42 -0300

    testes

--- basic_project/urls.py
+++ basic_project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
 def response_blog (request):
     print('can do other things')

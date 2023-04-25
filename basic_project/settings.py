375496a889900d4d05cf8cad0a982e1600379bb0
1King-coder <progvitor@gmail.com>
2023-04-24 23:41:17 -0300

    testing paths

--- basic_project/settings.py
+++ basic_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from secret_key import secret_key
+from .secret_key import secret_key
 SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
